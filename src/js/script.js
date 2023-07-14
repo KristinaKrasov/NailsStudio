87bb5b2874aa495e06456e38883b31b8d9685d58
Kristy <kristy010@yandex.ru>
2023-07-14 16:49:15 +0300

    add carusel policy services

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+    $('.carusel__wrapper').slick({
+        dots: false,
+        arrows: false,
+        slidesToShow: 3,
+        centerMode: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        infinite: true,
+        variableWidth: true
+    });
+  });
